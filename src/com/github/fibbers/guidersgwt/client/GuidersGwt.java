35694130fb8949505f66cdc23b427e2a216a1f9f
Willem Fibbe <fibbers@gmail.com>
2013-12-05 15:59:34 +0100

    The basic to start with
    Functional build script, an EntryPoint/module and a necessary resource
    
    This build works, because I placed gwt-{user,dev}.jar in war/WEB-INF/lib

---
+++ src/com/github/fibbers/guidersgwt/client/GuidersGwt.java
@@ -0,0 +1,11 @@
+package com.github.fibbers.guidersgwt.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class GuidersGwt implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+	}
+
+}
